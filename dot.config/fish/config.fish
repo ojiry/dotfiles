f8167dc7aa06525abcfc3ce2a3c9a399cc13aa1b
Ryoji Yoshioka <yoshioka.ryoji@gmail.com>
2016-05-27 13:09:29 +0900

    Update dotfiles

--- dot.config/fish/config.fish
+++ dot.config/fish/config.fish
@@ -1,6 +1,8 @@
 set -U PAGER less
 set -U XDG_CONFIG_HOME $HOME/.config
-set -x PATH $HOME/bin $PATH
+set -x OCI_DIR $HOME/opt/oracle/instantclient_11_2
+set -x GOPATH $HOME/work
+set -x PATH $HOME/bin $PATH $GOPATH/bin
 
 alias ag "ag --stats -S"
 alias g git
