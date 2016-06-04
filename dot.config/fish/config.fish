73c2459053ab83914a22c6945dcebf07e1dcc23d
Ryoji Yoshioka <yoshioka.ryoji@gmail.com>
2016-06-04 20:36:38 +0900

    Add docker env

--- dot.config/fish/config.fish
+++ dot.config/fish/config.fish
@@ -3,6 +3,10 @@ set -U XDG_CONFIG_HOME $HOME/.config
 set -x OCI_DIR $HOME/opt/oracle/instantclient_11_2
 set -x GOPATH $HOME/work
 set -x PATH $HOME/bin $PATH $GOPATH/bin
+set -gx DOCKER_TLS_VERIFY "1";
+set -gx DOCKER_HOST "tcp://192.168.99.100:2376";
+set -gx DOCKER_CERT_PATH "$HOME/.docker/machine/machines/default";
+set -gx DOCKER_MACHINE_NAME "default";
 
 alias ag "ag --stats -S"
 alias g git
