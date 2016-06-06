cbb0712a74b82a2fb4f1f8312338bafa4e30fc7d
Ryoji Yoshioka <yoshioka.ryoji@gmail.com>
2016-06-06 11:35:54 +0900

    Update config.fish

--- dot.config/fish/config.fish
+++ dot.config/fish/config.fish
@@ -1,13 +1,20 @@
+set -gx PATH $HOME/bin $PATH $GOPATH/bin
+
+set -U EDITOR vim
 set -U PAGER less
-set -U XDG_CONFIG_HOME $HOME/.config
-set -x ORACLE_HOME $HOME/opt/oracle
-set -x OCI_DIR $ORACLE_HOME/instantclient_11_2
-set -x GOPATH $HOME/work
-set -x PATH $HOME/bin $PATH $GOPATH/bin
+
 set -gx DOCKER_TLS_VERIFY "1";
 set -gx DOCKER_HOST "tcp://192.168.99.100:2376";
 set -gx DOCKER_CERT_PATH "$HOME/.docker/machine/machines/default";
 set -gx DOCKER_MACHINE_NAME "default";
+
+set -gx GOPATH $HOME/work
+set -gx PATH $PATH $GOPATH/bin
+
+set -gx ORACLE_HOME $HOME/opt/oracle
+set -gx OCI_DIR $ORACLE_HOME/instantclient_11_2
+
+set -gx XDG_CONFIG_HOME $HOME/.config
 
 alias ag "ag --stats -S"
 alias g git
