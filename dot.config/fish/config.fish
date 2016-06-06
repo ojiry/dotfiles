c44300e50fb40a5356435a281545ce69a69fd32d
Ryoji Yoshioka <yoshioka.ryoji@gmail.com>
2016-06-06 11:03:10 +0900

    Add ORACLE_HOME

--- dot.config/fish/config.fish
+++ dot.config/fish/config.fish
@@ -1,6 +1,7 @@
 set -U PAGER less
 set -U XDG_CONFIG_HOME $HOME/.config
-set -x OCI_DIR $HOME/opt/oracle/instantclient_11_2
+set -x ORACLE_HOME $HOME/opt/oracle
+set -x OCI_DIR $ORACLE_HOME/instantclient_11_2
 set -x GOPATH $HOME/work
 set -x PATH $HOME/bin $PATH $GOPATH/bin
 set -gx DOCKER_TLS_VERIFY "1";
