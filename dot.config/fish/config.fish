2d0ed348af25c36d07c2f8a83c709f6c33f2cb8b
Ryoji Yoshioka <yoshioka.ryoji@gmail.com>
2016-12-29 20:58:41 +0900

    Remove oracle env

--- dot.config/fish/config.fish
+++ dot.config/fish/config.fish
@@ -2,23 +2,12 @@ set -gx LANGUAGE en_US.UTF-8
 set -gx LANG $LANGUAGE
 set -gx LC_ALL $LANGUAGE
 set -gx LC_CTYPE $LANGUAGE
-
 set -gx PATH $HOME/bin $PATH $GOPATH/bin
-
 set -gx EDITOR vim
 set -gx PAGER less
-
 set -gx GOPATH $HOME/work
 set -gx PATH $PATH $GOPATH/bin
-
 set -gx FZF_DEFAULT_OPTS --reverse
-
-if test -d $HOME/instantclient_11_2
-  set -gx ORACLE_HOME $HOME/instantclient_11_2
-  set -gx OCI_DIR $ORACLE_HOME
-  set -gx PATH $ORACLE_HOME $PATH
-end
-
 set -gx XDG_CONFIG_HOME $HOME/.config
 
 alias ag "ag --stats -S"
@@ -32,4 +21,3 @@ alias r rails
 
 rbenv init - | source
 eval (direnv hook fish)
-
