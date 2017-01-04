fb42d3af7df0a7db8421c3a2366c1d06c6e086b5
Ryoji Yoshioka <yoshioka.ryoji@gmail.com>
2017-01-04 22:32:03 +0900

    Update config.fish

--- dot.config/fish/config.fish
+++ dot.config/fish/config.fish
@@ -1,24 +1,44 @@
+# Language
 set -gx LANGUAGE en_US.UTF-8
 set -gx LANG $LANGUAGE
 set -gx LC_ALL $LANGUAGE
 set -gx LC_CTYPE $LANGUAGE
-set -gx PATH $HOME/bin $PATH $GOPATH/bin
+
+# Editor
 set -gx EDITOR vim
+set -gx CVSEDITOR $EDITOR
+set -gx SVN_EDITOR $EDITOR
+set -gx GIT_EDITOR $EDITOR
+
+# Pager
 set -gx PAGER less
+
+# XDG
+set -gx XDG_CACHE_HOME $HOME/.cache
+set -gx XDG_CONFIG_HOME $HOME/.config
+set -gx XDG_DATA_HOME $HOME/.local/share
+
+# Golang
 set -gx GOPATH $HOME/work
 set -gx PATH $PATH $GOPATH/bin
+
+# fzf
 set -gx FZF_DEFAULT_OPTS --reverse
 set -gx FZF_LEGACY_KEYBINDINGS 0
-set -gx XDG_CONFIG_HOME $HOME/.config
 
+# rbenv
+rbenv init - | source
+
+# direnv
+eval (direnv hook fish)
+
+# Aliases
 alias ag "ag --stats -S"
 alias be "bundle exec"
-alias bi="bundle install"
-alias bo="bundle open"
-alias bu="bundle update"
-alias emacs nvim
+alias bi "bundle install"
+alias bo "bundle open"
+alias bu "bundle update"
+alias du "du -h"
 alias g git
 alias r rails
-
-rbenv init - | source
-eval (direnv hook fish)
+alias vi vim
