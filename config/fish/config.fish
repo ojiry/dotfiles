aecc6f145d72eec2043b76b2d60c26212a179b4a
Ryoji Yoshioka <yoshioka.ryoji@gmail.com>
2019-05-15 10:58:04 +0900

    Remove unnecessary environment variable

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -22,9 +22,6 @@ set -gx XDG_CACHE_HOME $HOME/.cache
 set -gx XDG_CONFIG_HOME $HOME/.config
 set -gx XDG_DATA_HOME $HOME/.local/share
 
-# Homebrew
-set -gx HOMEBREW_INSTALL_CLEANUP 1
-
 # Golang
 set -gx GOPATH $HOME/go
 set -gx PATH $PATH $GOPATH/bin
